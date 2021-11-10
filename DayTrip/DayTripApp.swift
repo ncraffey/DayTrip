3c42791cf2105fd64e521448b59f2c40c31f2240
Nick Craffey <craffeyni@gmail.com>
2021-11-10 13:11:50 -0500

    Initial Commit

---
+++ DayTrip/DayTripApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayTripApp.swift
+//  DayTrip
+//
+//  Created by Nicholas Craffey on 11/10/21.
+//
+
+import SwiftUI
+
+@main
+struct DayTripApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
